0040e6137ea17631c52bced287944d695f5ef5d1
Aung Min Oo <aungminoo1314@gmail.com>
2021-06-18 21:47:59 +0900

    delete post form firebase

--- src/components/SinglePost.vue
+++ src/components/SinglePost.vue
@@ -1,8 +1,15 @@
 <template>
     
-    <router-link :to="{name:'Detail',params:{id:post.id}}">
-        <h2 class="text-capitalize">{{post.title}}</h2>
-    </router-link>
+    <div class="title">
+        <div>
+            <router-link :to="{name:'Detail',params:{id:post.id}}">
+                <h2 class="text-capitalize">{{post.title}}</h2>
+            </router-link>
+        </div>
+        <div class="delete-btn">
+            <button @click="deletePost">delete</button>
+        </div>
+    </div>
     <div v-for="tag in post.tags" :key="tag" class="d-inline mr-2">
         <span class="badge badge-pill badge-primary p-1">
             <!-- tag:tag=tag -->
@@ -14,18 +21,43 @@
 
 <script>
 import { computed } from '@vue/runtime-core'
+import {db} from "../firebase/config"
+import {useRouter} from "vue-router"
+
 export default {
     props:["post"],
     setup(props){
         let cutPostBody = computed(()=>{
             return props.post.body.substring(0,100)+"...";
         })
-        return {cutPostBody};
+
+        let router = useRouter();
+        let deletePost = async()=>{
+            let id = props.post.id;
+            await db.collection("posts").doc(id).delete();
+            router.push("/")
+        }
+
+        return {cutPostBody,deletePost};
     }
 }
 </script>
 
 <style>
+    .title{
+        display: flex;
+        justify-content: space-between;
+        align-content: center;
+    }
+    .title .delete-btn button{
+        border-radius: 10px;
+        background: red;
+        color: black;
+        font-weight: bold;
+        border: none;
+        padding: 0 10px;
+        text-transform: uppercase;
+    }
     span a{
         color: white;
         /* margin-right: 20px; */
